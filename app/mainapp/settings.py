8625923fe95a143e46e38b8eb5652c2b504ebfaa
Emanuele Disco <emawind84@gmail.com>
2020-07-06 14:20:37 +0900

    update

--- app/mainapp/settings.py
+++ app/mainapp/settings.py
@@ -149,9 +149,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ]
 }
 
 try:
